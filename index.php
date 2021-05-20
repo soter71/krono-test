413d7265cb25cdabffdbc1b7214bc01be22e4308
soter71 <sogiot@gmail.com>
2021-05-21 02:13:20 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <title>Countdown Timer | sg555.gr</title>
+  <link href="https://fonts.googleapis.com/css?family=Lato|Montserrat|Rubik+Mono+One&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+  <div class="main">
+    <h1>Selected Date Is 7<sup>th</sup> Of August</h1>
+
+    <div id="countdown"></div>
+  </div>
+
+  <script src="function.js"></script>
+
+</body>
+
+</html>
