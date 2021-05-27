4973380501dba400279003e093c484c5ec5a8e38
soter71 <sogiot@gmail.com>
2021-05-27 18:47:07 +0300

    t1

--- index.php
+++ index.php
@@ -1,24 +1,26 @@
 <!DOCTYPE html>
-
-<html>
+<html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <title>Countdown Timer | sg555.gr</title>
-  <link href="https://fonts.googleapis.com/css?family=Lato|Montserrat|Rubik+Mono+One&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Lato|Montserrat|Rubik+Mono+One&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+    
 </head>
 
 <body>
-  <div class="main">
-    <h1>Selected Date Is 18<sup>th</sup> Of August 2021</h1>
-    <h2>at 16:00</h2>
+    <div class="main">
+        <h1>Selected Date Is 18<sup>th</sup> Of August 2021</h1>
+        <h2>at 16:00</h2>
 
-    <div id="countdown"></div>
-  </div>
+        <div id="countdown"></div>
+    </div>
 
-  <script src="function.js"></script>
-
+    <script src="function.js"></script>
 </body>
 
 </html>
